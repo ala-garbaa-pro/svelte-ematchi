1a2ce931faa65c1fb98097c869d0326ab8288d1c
PAD <79337368+ala-garbaa-pro@users.noreply.github.com>
2024-01-18 20:15:25 +0100

    Designing Styles for Game Interfaces

---
+++ src/routes/Game.svelte
@@ -0,0 +1,26 @@
+<div class="game">
+	<div class="info"></div>
+	<div class="grid-container"></div>
+	<div class="info"></div>
+</div>
+
+
+<style>
+    .game {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+    .info {
+        width: 80vmin;
+        height: 10vmin;
+        background-color: goldenrod;
+    }
+    .grid-container {
+        width: 80vmin;
+        height: 80vmin;
+        background-color: teal;
+    }
+</style>
